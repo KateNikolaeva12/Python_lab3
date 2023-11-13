b4470fc223ea1d324f4767ad9d84d235d609040f
KateNikolaeva12 <147624107+KateNikolaeva12@users.noreply.github.com>
2023-11-13 13:44:18 +0300

    Add files via upload

---
+++ Лабораторная работа 4/task_1.py
@@ -0,0 +1,15 @@
+import json
+
+INPUT = 'input.json'
+
+
+def task() -> float:
+    with open(INPUT) as file:
+        s = 0
+        slovar = json.load(file)
+        for i in slovar:
+            s += i['score'] * i['weight']
+    return round(s, 3)
+
+
+print(task())
