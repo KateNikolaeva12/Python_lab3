466616d24850363ee81f1f0864fd26bf4f87f1d7
KateNikolaeva12 <147624107+KateNikolaeva12@users.noreply.github.com>
2023-11-16 21:28:53 +0300

    Update task_1.py

--- Лабораторная работа 4/task_1.py
+++ Лабораторная работа 4/task_1.py
@@ -5,11 +5,9 @@ INPUT = 'input.json'
 
 def task() -> float:
     with open(INPUT) as file:
-        s = 0
         slovar = json.load(file)
-        for i in slovar:
-            s += i['score'] * i['weight']
-    return round(s, 3)
+        s = [num['score'] * num['weight'] for num in slovar]
+    return round(sum(s), 3)
 
 
 print(task())
